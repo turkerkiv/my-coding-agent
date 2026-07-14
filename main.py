fa61ceacfdc841f9c45581daaa25e629ef79ed95
Türker Kıvılcım <kivturker@gmail.com>
2026-07-14 11:47:12 +0300

    add getting input prompt from users

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
+import argparse
 
 def main():
     print("Hello from my-coding-agent!")
@@ -16,22 +17,28 @@ def main():
         api_key=api_key,
     )
 
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [
+        { "role": "user", "content": args.user_prompt }
+    ]
+
     response = client.chat.completions.create(
         model = "openrouter/free",
-        messages = [
-            {
-                "role": "user",
-                "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-            }
-        ]
+        messages = messages,
     )
 
     if response.usage is None:
         raise RuntimeError("Failed API request")
 
-    print(f"Prompt tokens: {response.usage.prompt_tokens}")
-    print(f"Response tokens: {response.usage.completion_tokens}")
-    
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage.prompt_tokens}")
+        print(f"Response tokens: {response.usage.completion_tokens}")
+
     print(response.choices[0].message.content)
 
 if __name__ == "__main__":
