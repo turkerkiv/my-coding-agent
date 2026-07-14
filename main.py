ad170fa708e31f93f2310b4252725faf3d2e07a9
Türker Kıvılcım <kivturker@gmail.com>
2026-07-14 11:24:32 +0300

    add test connections

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
 def main():
     print("Hello from my-coding-agent!")
 
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+
+    if api_key is None:
+        raise RuntimeError("api key is not found")
+
+    client = OpenAI(
+        base_url = "https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+
+    response = client.chat.completions.create(
+        model = "openrouter/free",
+        messages = [
+            {
+                "role": "user",
+                "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+            }
+        ]
+    )
+
+    if response.usage is None:
+        raise RuntimeError("Failed API request")
+
+    print(f"Prompt tokens: {response.usage.prompt_tokens}")
+    print(f"Response tokens: {response.usage.completion_tokens}")
+    
+    print(response.choices[0].message.content)
 
 if __name__ == "__main__":
     main()
