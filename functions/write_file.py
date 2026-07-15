348bd58e7e6a83ee6850569f519c647c6083581d
Türker Kıvılcım <kivturker@gmail.com>
2026-07-15 13:13:47 +0300

    add write_file function with error handling and tests

---
+++ functions/write_file.py
@@ -0,0 +1,23 @@
+import os
+
+def write_file(working_directory: str, file_path: str, content: str) -> str:
+    try:
+        absolute_path = os.path.abspath(working_directory)
+
+        full_path = os.path.join(absolute_path, file_path)
+        normalized_target_path = os.path.normpath(full_path)
+
+        if os.path.isdir(normalized_target_path):
+            return f'Error: Cannot write to "{file_path}" as it is a directory'
+
+        is_valid_path = os.path.commonpath([absolute_path, normalized_target_path]) == absolute_path
+        if not is_valid_path:
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        else:
+            parents_path = os.path.join(absolute_path, os.path.dirname(file_path))
+            os.makedirs(parents_path, exist_ok=True)
+            with open(normalized_target_path, "w") as f:
+                length_written = f.write(content)
+            return f'Successfully wrote to "{file_path}" ({length_written} characters written)'
+    except Exception as e:
+        return f'Error: {e}'
