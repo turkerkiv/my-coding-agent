1ca65a6a5a957a0d1d25c88eb2060444560d0d3c
Türker Kıvılcım <kivturker@gmail.com>
2026-07-15 13:47:50 +0300

    add return detailed file information; update tests for consistency

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,18 +2,22 @@ import os
 
 def get_files_info(working_directory: str, directory: str = ".") -> str:
     try:
-        if os.path.isfile(directory):
-            return f'Error: "{directory}" is not a directory'
-         
         absolute_path = os.path.abspath(working_directory)
 
         full_path = os.path.join(absolute_path, directory)
         normalized_target_path = os.path.normpath(full_path)
 
-        is_valid_dir = os.path.commonpath([absolute_path, normalized_target_path]) == absolute_path
-        if not is_valid_dir:
+        if not os.path.isdir(normalized_target_path):
+            return f'Error: "{directory}" is not a directory'
+
+        is_valid_path = os.path.commonpath([absolute_path, normalized_target_path]) == absolute_path
+        if not is_valid_path:
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
         else:
-            return f'Success: "{directory}" is within the working directory'
+            dir_items = []
+            for filename in os.listdir(normalized_target_path):
+                full_file_path = os.path.join(normalized_target_path, filename)
+                dir_items.append(f"- {filename}: file_size={os.path.getsize(full_file_path)} bytes, is_dir={os.path.isdir(full_file_path)}")
+            return "\n".join(dir_items)
     except:
         return f'Error: unexpected error'
